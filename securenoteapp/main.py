41eafa10c6a207e82eecb26bc496b6ccd494ef37
Hubert Mazur <hmazur@griddynamics.com>
2023-01-06 23:21:59 +0100

    Improve creation of new notes

--- securenoteapp/main.py
+++ securenoteapp/main.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, current_app, redirect, url_for, flash, session
 from flask_login import login_required, current_user
+from sqlalchemy.sql import text
+from .models import Note
+from . import db
 
 main = Blueprint('main', __name__)
 
@@ -10,4 +13,31 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    res = Note.query.filter_by(owner_id = current_user.id).all()
+    current_app.logger.debug('%s', res)
+    
+    note_text = ""
+    if session.get('wip_note_text'):
+        note_text = session.get('wip_note_text')
+
+    return render_template('profile.html', name=current_user.name, note_text=note_text)
+
+@main.route('/note', methods=['POST'])
+@login_required
+def note_post():
+    title = request.form['title']
+    content = request.form['content']
+
+    if not title:
+        flash("Title cannot be empty")
+        session['wip_note_text'] = content
+        return redirect(url_for('main.profile'))
+    else:
+        session['wip_note_text'] = ""
+
+    new_note = Note(owner_id=current_user.id, title=title, content=content)
+
+    db.session.add(new_note)
+    db.session.commit()
+
+    return redirect(url_for('main.profile'))
--- securenoteapp/models.py
+++ securenoteapp/models.py
@@ -1,10 +1,27 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text, DateTime
 from flask_login import UserMixin
 from . import db
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    email = Column(String(100), unique=True)
-    password = Column(String(100))
-    name = Column(String(1000))
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    name = Column(String(1000), nullable=False)
+
+class Note(db.Model):
+    __tablename__ = "notes"
+    id = Column(Integer, primary_key=True)
+    owner_id = Column(ForeignKey("users.id"), nullable=False)
+    title = Column(String(100), nullable=False, default="Untitled note")
+    content = Column(Text, nullable=False, default="")
+    is_public = Column(Boolean, default=False, nullable=False)
+    is_encrypted = Column(Boolean, default=False, nullable=False)
+    password = Column(String(100), nullable=True)
+    public_link = Column(String(200), unique=True, nullable=True)
+
+class Share(db.Model):
+    __tablename__ = "shares"
+    id = Column(Integer, primary_key=True)
+    note_id = Column(ForeignKey("notes.id"))
+    viewer_id = Column(ForeignKey("users.id"))
